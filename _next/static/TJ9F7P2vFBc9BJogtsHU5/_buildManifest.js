0ca2ad6517037f49da952fc559f1882a98a0f552
hashirshoaeb <hashirshoaeb@gmail.com>
2023-01-22 08:26:06 +0500

    initial commit

---
+++ _next/static/TJ9F7P2vFBc9BJogtsHU5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/973-f31c9dcdb50bb0db.js","static/css/5674832e1468d24d.css","static/chunks/pages/index-2df8a30cef5a7d2f.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
